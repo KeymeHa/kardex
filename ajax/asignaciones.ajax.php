4b6cf701c2345aba3e150716e490b371ae8b96bf
Kevin B <86898763+KeymeHa@users.noreply.github.com>
2022-07-07 17:08:41 -0500

    8.1 - Asignaciones
    Habilita o no un usuario a los modulos de requisición o correspondencia

---
+++ ajax/asignaciones.ajax.php
@@ -0,0 +1,33 @@
+<?php
+require_once "../controladores/asignaciones.controlador.php";
+require_once "../modelos/asignaciones.modelo.php";
+
+class AjaxAsignacion
+{
+	public $id;
+	public $modulo;
+
+	public function ajaxHabilitar($accion)
+	{
+		if ($accion == 0) 
+		{
+			$respuesta = ControladorAsignaciones::ctrDeshabilitarUsuario($this->id, $this->modulo);
+		}
+		else
+		{
+			$respuesta = ControladorAsignaciones::ctrHabilitarUsuario($this->id, $this->modulo);
+		}
+
+		
+	}
+	
+}
+
+$hab = new AjaxAsignacion();
+
+if(isset($_POST["modulo"])){
+	#hab Habilitar
+	$hab -> id = $_POST["id"];
+	$hab -> modulo = $_POST["modulo"];
+	$hab -> ajaxHabilitar($_POST["accion"]);
+}
