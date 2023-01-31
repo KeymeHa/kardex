38eafa656d77147a1af0a613e070bafba4d646d2
Kevin B <86898763+KeymeHa@users.noreply.github.com>
2023-01-31 11:40:02 -0500

    Update kardex.sql

--- kardex.sql
+++ kardex.sql
@@ -102,6 +102,10 @@
 
 -- La exportación de datos fue deseleccionada.
 
+-- La exportación de datos fue deseleccionada.
+
+-- La exportación de datos fue deseleccionada.
+
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
