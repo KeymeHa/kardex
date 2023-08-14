09d288293e0bc100f5ac25c3d87a0e9030e94d2d
Kevin B <86898763+KeymeHa@users.noreply.github.com>
2023-08-14 10:10:12 -0500

    Update verpc.php
    timeline

--- vistas/modulos/verpc.php
+++ vistas/modulos/verpc.php
@@ -8,11 +8,13 @@
     }
     else
     {
-      $equipo = ControladorEquipos::ctrMostrarEquipos("id",$_GET["idpc"]);
-      $usuario = ControladorUsuarios::ctrMostrarNombre("id", $equipo["id_usuario"]);
-      $supervisor = ControladorUsuarios::ctrMostrarNombre("id", $equipo["id_responsable"]);
-      $usr_gen = ControladorUsuarios::ctrMostrarNombre("id", $equipo["id_usr_generado"]);
-      $area = ControladorAreas::ctrMostrarNombreAreas("id", $equipo["id_area"]);
+      $item = "id";
+      $equipo = ControladorEquipos::ctrMostrarEquipos($item,$_GET["idpc"]);
+      $usuario = ControladorUsuarios::ctrMostrarNombre($item, $equipo["id_usuario"]);
+      $supervisor = ControladorUsuarios::ctrMostrarNombre($item, $equipo["id_responsable"]);
+      $usr_gen = ControladorUsuarios::ctrMostrarNombre($item, $equipo["id_usr_generado"]);
+      $area = ControladorAreas::ctrMostrarNombreAreas($item, $equipo["id_area"]);
+      $proyecto = ControladorProyectos::ctrMostrarProyectos($item, $equipo["id_proyecto"]);
 
       if (is_null($equipo)) 
       {
@@ -77,13 +79,6 @@
               <div class="col-lg-6 col-md-6 col-sm-12">
                 <dl class="dl-horizontal">
                   <?php 
-                    #asignado a
-                    #rol
-                    #Responsable
-                    #area
-                    #generado por
-                    #propietario
-                    #nombrepc
                     echo ControladorEquipos::ctrMostrarItem($equipo["n_serie"], 0, "Serial");
                     echo ControladorEquipos::ctrMostrarItem($equipo["nombre"], 0, "Nombre PC");
                     echo ControladorEquipos::ctrMostrarItem($equipo["serialD"], 0, "2do Serial");
@@ -160,13 +155,13 @@
                 <div class="col-sm-3 col-xs-3 col-lg-3 col-md-3">
                   <div class="description-block border-right">
                     <span class="description-text">Proyecto</span>
-                    <h5 class="description-header">Administrativo</h5>
+                    <h5 class="description-header"><?php echo $proyecto["nombre"];?></h5>
                   </div>
                 </div>
 
               </div><!--box-body-->
               <div class="box-footer">
-                 <button class="btn btn-success"><i class="fa fa-user-plus"></i> Reasignar</button>
+                 <button class="btn btn-success btn-reasignar" id="btn-reasignar" data-toggle="modal" data-target="#modalReasignarPC"><i class="fa fa-user-plus"></i> Reasignar</button>
               </div>
             </div>
 
@@ -246,4 +241,186 @@
    
   </section>
 
+</div>
+
+
+
+<div id="modalReasignarPC" class="modal fade" role="dialog">
+   <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <form role="form" method="post">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+        <div class="modal-header" style="background:#00A65A; color:white">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Reasignar Equipo:<strong><?php echo $equipo["nombre"]."</strong>, Serial: <strong>".$equipo["n_serie"];?></strong> </h4>
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+          <div class="box-body">
+
+            <div class="divAlertError">
+              
+            </div>
+
+
+            <div class="row">
+
+
+              <div class="col-md-12 col-lg-12 col-sm-12">
+                <h4>Responsabilidad</h4>
+              </div>
+
+              <div class="col-md-6 col-lg-6 col-sm-12">
+             
+             
+                <div class="form-group">
+                  <label>Responsable</label>
+                  <select class="form-control" name="selectResponsableE">
+                    <?php
+                      $responsable = ControladorPersonas::ctrMostrarPersonas("sw", 1);
+
+                      $contar = 0;
+                      $gatillo = 0;
+
+                      while ( $contar < count($responsable) && $gatillo == 0 ) 
+                      {
+                        if ( $responsable[$contar]["id"] == $equipo["id_responsable"] ) 
+                        {
+                          $areaR = ControladorAreas::ctrMostrarAreas("id", $responsable[$contar]["id_area"]);
+                          echo '<option value="'.$responsable[$contar]["id"].'">'.$responsable[$contar]["nombre"].' - '.$areaR["nombre"].'</option>';
+                          $gatillo = 1;
+                        }
+                        $contar++;
+                      }
+
+                      foreach ($responsable as $key => $value) :
+                        $areaR = ControladorAreas::ctrMostrarAreas("id", $value["id_area"]);
+
+                        if ($equipo["id_responsable"] != $value["id"]) 
+                        {
+                           echo '<option value="'.$value["id"].'">'.$value["nombre"].' - '.$areaR["nombre"].'</option>';
+                        }
+                      endforeach;
+                    ?>
+                  </select>
+                </div>
+              </div>
+
+
+              <div class="col-md-6 col-lg-6 col-sm-12">
+                <div class="form-group">
+                  <label>Asignado a:</label>
+                  <select class="form-control" name="selectAsignadoE">
+                    <?php
+
+                    $usuarios = ControladorUsuarios::ctrMostrarUsuarios(null, null);
+
+                    $contar = 0;
+                    $gatillo = 0;
+
+                    while ( $contar < count($usuarios) && $gatillo == 0 ) 
+                    {
+                      if ( $usuarios[$contar]["id"] == $equipo["id_usuario"] ) 
+                      {
+                        echo '<option value="'.$usuarios[$contar]["id"].'">'.$usuarios[$contar]["nombre"].'</option>';
+                        $gatillo = 1;
+                      }
+                      $contar++;
+                    }
+
+                    foreach ($usuarios as $key => $value) 
+                    {
+                      if ($equipo["id_usuario"] != $value["id"]) 
+                      {
+                         echo '<option value="'.$value["id"].'">'.$value["nombre"].'</option>';
+                      }
+                    }
+
+                    ?>
+                  </select>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-6 col-sm-12">
+                <div class="form-group">
+                  <label>Rol</label>
+                  <select class="form-control" name="selectRolE">
+
+                    <?php echo ( $equipo["rol"] == 0 ) ? '<option value="0">Contratista</option><option value="1">Empleado</option>' : '<option value="1">Empleado</option><option value="0">Contratista</option>' ; ?>
+
+                    
+                  </select>
+                </div>
+              </div>
+
+              <div class="col-md-6 col-lg-6 col-sm-12">
+                <div class="form-group">
+                  <label>Proyecto:</label>
+                  <select class="form-control" name="selectProyectoE">
+                    <?php
+
+                    $proyectos = ControladorProyectos::ctrMostrarProyectos(null, null);
+
+                    foreach ($proyectos as $key => $value) 
+                    {
+                      echo '<option value="'.$value["id"].'">'.$value["nombre"].'</option>';
+                    }
+
+                    ?>
+                  </select>
+                </div>
+              </div>
+
+            </div><!--row-->
+
+            <div class="col-md-6 col-lg-6 col-sm-12">
+                <div class="form-group">
+                  <label>*Fecha asignación:</label>
+                  <div class="input-group date">
+                    <div class="input-group-addon">
+                      <i class="fa fa-calendar"></i>
+                    </div>
+                    <input type="date" class="form-control" name="dateReasignar" id="dateReasignar" value="" />
+                  </div>
+                </div>
+              </div><!--col-md-6 col-lg-6 col-sm-12-->
+
+
+
+            <div class="col-lg-12 col-md-12 col-sm-12">
+              <div class="form-group">
+                <label>Observaciones</label>
+                <textarea class="form-control" rows="3" placeholder="Enter ..." name="textObservacionesE"></textarea>
+              </div>
+            </div>
+
+          </div><!--box-body-->
+        </div><!--modal-body-->
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger pull-left" data-dismiss="modal">Salir</button>
+          <button type="submit" class="btn btn-success">Ingresar</button>
+        </div>
+
+        <?php
+
+        $accionEquipo = new ControladorEquipos();
+        $accionEquipo -> ctrAccionEquipo($_SESSION["id"]);
+
+        ?>
+
+      </form>
+    </div>
+  </div>
 </div>
