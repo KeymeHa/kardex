983b6fa75f3167ef5858038ab8f894f38f5d1faa
Kevin B <86898763+KeymeHa@users.noreply.github.com>
2023-08-22 11:48:47 -0500

    Update verpc.js
    Llenar el formulario para editar equipo

--- vistas/js/verpc.js
+++ vistas/js/verpc.js
@@ -3,6 +3,37 @@ $("button.btn-reasignar").click(function(){
 	hoy(elemento);
 });
 
+$("#pc_serial").change(function() {
+
+	var n_serie = $(this).val();
+
+	var datos = new FormData();
+	datos.append("n_serie", n_serie);
+
+	$(".alert").remove();
+
+	$.ajax({
+
+		url:"ajax/equipos.ajax.php",
+		method: "POST",
+		data: datos,
+		cache: false,
+		contentType: false,
+		processData: false,
+		dataType: "json",
+		success: function(respuesta){	
+
+			if(respuesta)
+			{
+				$("#pc_serial").parent().after('<div class="alert alert-warning"><i class="fa  fa-info"></i> '+n_serie+' ya existe ! .</div>');
+	    		$("#pc_serial").val("");
+	    		ocultarAlert();
+    		}	
+		}
+	});
+
+
+});
 
 
 $("button.btn-editarPC").click(function() {
@@ -23,6 +54,7 @@ $("button.btn-editarPC").click(function() {
 		dataType: "json",
 		success: function(response1)
 		{	
+
 			$("#pc_serial").val(response1["n_serie"]);
 			$("input.inputEquipoAccion").val(idPC);
 			$("input.inputSerialDE").val(response1["serialD"]);
@@ -58,7 +90,8 @@ $("button.btn-editarPC").click(function() {
 			}
 
 
-				$(".selectIdCPUGenE").append('<option value="'+response1["cpu_generacion"]+'">'+response1["cpu_generacion"]+'</option>');
+			$(".selectIdCPUGenE").append('<option value="'+response1["cpu_generacion"]+'">'+response1["cpu_generacion"]+'</option>');
+			
 			for (var i = 12; i >= 4; i--) {
 				if (response1["cpu_generacion"] != i) 
 				{
@@ -67,12 +100,14 @@ $("button.btn-editarPC").click(function() {
 				
 			}
 
-			var elementos = [ 'selectIdProE', 'selectIdArqE', 'selectIdMarcaE', 'selectIdModeloE',
-			 'selectIdCPUE', 'selectIdCPUModE', 'selectSOE', 'selectSOVerE', 'selectIdActaE', 'selectResponsableE', 'selectAsignadoE',
-			 'selectProyectoE', 'selectLicenciaE'];
+			var elementos = [ "selectIdProE", "selectIdArqE", "selectIdMarcaE", "selectIdModeloE",
+			 "selectIdCPUE", "selectIdCPUModE", "selectSOE", "selectSOVerE", "selectIdActaE", "selectResponsableE", "selectAsignadoE",
+			 "selectProyectoE", "selectLicenciaE"];
+
 			var llaves = [ 'id_propietario', 'id_arquitectura', 'marca', 'modelo',
 			 'cpu', 'cpu_modelo', 'so', 'so_version', 'id_acta', 'id_responsable',
 			  'id_usuario', 'id_proyecto', 'id_licencia' ];
+
 			var valores = [ response1['id_propietario'], response1['id_arquitectura'], response1['marca'], 
 			response1['modelo'], response1['cpu'], response1['cpu_modelo'], 
 			response1['so'], response1['so_version'], response1['id_acta'], 
@@ -83,6 +118,7 @@ $("button.btn-editarPC").click(function() {
 				var datas = new FormData();
 				datas.append("item" , llaves[i]);
 				datas.append("valor" , valores[i]);
+				datas.append("elemento" , elementos[i]);
 				datas.append("datosSelect", 1);
 
 				$.ajax({
@@ -96,15 +132,170 @@ $("button.btn-editarPC").click(function() {
 					dataType: "json",
 					success: function(response2)
 					{
-						for (var j = 0; j < response2.length; j++) 
+							console.log(" en #"+response2.length);
+						
+
+						for (var j = 0; j < response2.length-1; j++) 
 						{
-							$("select."+elementos[i]).append('<option value="'+response2[j]['id']+'">'+response2[j]['nombre']+'</option>');
-						}
+							$("#"+response2[response2.length-1]).append('<option value="'+response2[j]["id"]+'">'+response2[j]["nombre"]+'</option>');
+
+						}//for (var j = 0; j < arrI.length; j++)
 					}
 				});
+	
 			}
 
 		}
 	});
 
 });
+
+
+$("button.btn-addParam").click(function(){
+	var param = $(this).attr("param");
+	$(this).parent().parent().parent().children('div.div-add').children().remove();
+	$(this).parent().parent().parent().children('div.div-add').append('<div class="col-lg-12 col-md-12 col-sm-12">'+
+                  '<div class="form-group form-groupAlert'+param+'" >'+
+                    '<label class="control-label"><i class="fa fa-pencil"></i> Nuevo Valor</label>'+
+                    '<input type="text" id="inputParam'+param+'" value="" class="form-control inputParam" placeholder="Enter ..." param="'+param+'">'+
+                    '<div class="btn-group">'+
+                      '<button type="button" class="btn btn-xs btn-success btn-saveParam"  param="'+param+'"><i class="fa fa-save"></i> Guardar</button>'+
+                      '<button type="button" class="btn btn-xs btn-danger btn-cancelParam"><i class="fa fa-close"></i> Cancelar</button>'+
+                    '</div>'+
+                    '</div>'+
+                '</div>');
+});
+
+$("form").on('click', 'button.btn-saveParam', function(){
+	
+	var tipo = $(this).attr("param");
+	var valor = $(this).parent().parent().children('input.form-control').val();
+	var idSession = $("#inputPagCarAnioActual").attr("idSession");
+
+	//buscar si existe
+
+		if(valor != "")
+		{
+			$("div.divAlertError > div.alert-warning").remove();
+			var datos = new FormData();
+			datos.append("valor", valor);
+			datos.append("tipo", tipo);
+			datos.append("idSession", idSession);
+
+			$.ajax({
+
+				url:"ajax/equipos.ajax.php",
+				method: "POST",
+				data: datos,
+				cache: false,
+				contentType: false,
+				processData: false,
+				dataType: "json",
+				success: function(respuesta){	
+
+					if(respuesta == 1)
+					{
+						$("div.form-groupAlert"+tipo).addClass('has-error');
+						$("div.divAlertError").append('<div class="alert alert-warning"><i class="fa  fa-info"></i> El Parametro ya existe.</div>');
+		    			$("#inputParam"+tipo).val("");
+		    			ocultarAlert();
+		    		}	
+		    		else
+		    		{
+		    			var selectAdd = $("#inputParam"+tipo).parent().parent().parent().parent().children('div.input-group').children('select.form-control');//ok
+		    			$("#inputParam"+tipo).val("");//ok
+		    			$(selectAdd).children().remove();//ok
+		    			$("#inputParam"+tipo).parent().parent().remove();//ok
+	    			
+						
+
+						var datosDos = new FormData();
+						datosDos.append("addParam", 1);
+						datosDos.append("valor", valor);
+						datosDos.append("tipo", tipo);
+						datosDos.append("idSession", idSession);
+						$.ajax({
+							url:"ajax/equipos.ajax.php",
+							method: "POST",
+							data: datosDos,
+							cache: false,
+							contentType: false,
+							processData: false,
+							dataType: "json",
+							success: function(respuestaD)
+							{	
+
+								for (var i = 0; i < respuestaD.length; i++) 
+								{
+									if (valor == respuestaD[i]['nombre']) 
+									{
+										$(selectAdd).append('<option value="'+respuestaD[i]['id']+'">'+respuestaD[i]['nombre']+'</option>');
+									}
+
+								}
+
+								for (var i = 0; i < respuestaD.length; i++) 
+								{
+									if (valor != respuestaD[i]['nombre']) 
+									{
+										$(selectAdd).append('<option value="'+respuestaD[i]['id']+'">'+respuestaD[i]['nombre']+'</option>');
+									}
+
+								}
+							}
+						});
+		    			//quitar opcion del select
+		    			//traer todos los parametros de ese tipo
+		    			//agregar primer option el valor ingresado
+		    			//agregar los demas valores si no es el valor anteriormente ingresado
+						
+		    		}
+				}
+			});
+		}//if(valor != "")
+	//mostrar mensaje de error
+});
+
+$("form").on('click', 'button.btn-cancelParam', function(){
+	$(this).parent().parent().parent().remove();
+});
+
+
+$("form").on('change', 'input.inputParam', function(){
+	var valor = $(this).val();
+	var tipo = $(this).attr("param");
+
+	//buscar si existe
+
+		$(".alert").remove();
+
+		var datos = new FormData();
+		datos.append("valor", valor);
+		datos.append("tipo", tipo);
+
+		$.ajax({
+
+			url:"ajax/equipos.ajax.php",
+			method: "POST",
+			data: datos,
+			cache: false,
+			contentType: false,
+			processData: false,
+			dataType: "json",
+			success: function(respuesta){	
+
+				if(respuesta == 1)
+				{
+					$("div.form-groupAlert"+tipo).addClass('has-error');
+					$("div.divAlertError").append('<div class="alert alert-warning"><i class="fa  fa-info"></i> El Parametro ya existe.</div>');
+	    			$("#inputParam"+tipo).val("");
+	    			ocultarAlert();
+	    		}
+			}
+		});
+
+		
+
+	//mostrar mensaje de error
+});
+
