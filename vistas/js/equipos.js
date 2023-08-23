6b7f06dd5d9e356ddf3ab867de2df1812401b35b
Kevin B <86898763+KeymeHa@users.noreply.github.com>
2023-08-23 16:49:17 -0500

    Update equipos.js
    Notificar que el equipo tiene un nombre repetido.

--- vistas/js/equipos.js
+++ vistas/js/equipos.js
@@ -2,6 +2,8 @@ $("button.btn-newEquipo").click(function(){
 	$("#inputEquipoAccion").val(0);
 	var elemento = $("#dateIngresoE");
 	hoy(elemento);
+	$(".btn-modal").html("Agregar");
+	$(".titulo-modal").html("Nuevo Equipo");
 });
 
 $("#pc_nombreE").change(function() {
@@ -27,9 +29,14 @@ $("#pc_nombreE").change(function() {
 
 			if(respuesta)
 			{
-				$("#pc_nombreE").parent().after('<div class="alert alert-warning"><i class="fa  fa-info"></i> '+pc_nombre+' ya existe ! .</div>');
-	    		$("#pc_nombreE").val("");
-	    		ocultarAlert();
+				if (respuesta["estado"] == 1) 
+				{
+					$("#pc_nombreE").parent().after('<div class="alert alert-warning alert-dismissible">'+
+						'<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button><i class="fa  fa-info"></i> '+pc_nombre+' ya existe en el equipo '+respuesta["n_serie"]+
+						'</div>');
+		    		//$("#pc_nombreE").val("");
+		    		//ocultarAlert();
+				}
     		}	
 		}
 	});
@@ -224,6 +231,129 @@ $("form").on('change', 'input.inputParam', function(){
 	//mostrar mensaje de error
 });
 
+
+$("table.tablaEquipos").on('click', 'button.btn-editarPC', function() 
+{
+
+	$(".alert").remove();
+	
+	var idPC = $(this).attr("idPC");
+	var nombre = $(this).attr("nombre");
+	var datos = new FormData();
+	datos.append("idPC", idPC);
+
+	$(".btn-modal").html("Editar");
+	$(".titulo-modal").html("Editar equipo: "+nombre);
+
+	$("input.inputEquipoAccion").val(idPC);
+
+	$.ajax({
+
+		url:"ajax/equipos.ajax.php",
+		method: "POST",
+		data: datos,
+		cache: false,
+		contentType: false,
+		processData: false,
+		dataType: "json",
+		success: function(response1)
+		{	
+
+			$("#pc_serial").val(response1["n_serie"]);
+			$("input.inputEquipoAccion").val(idPC);
+			$("input.inputSerialDE").val(response1["serialD"]);
+			$("#pc_nombreE").val(response1["nombre"]);
+			$("input.inputCPUFreE").val(response1["cpu_frecuencia"]);
+			$("input.inputRamE").val(response1["ram"]);
+			$("input.inputSSDE").val(response1["ssd"]);
+			$("input.inputHDDE").val(response1["hdd"]);
+			$("input.inputGPUE").val(response1["gpu"]);
+			$("input.inputGPUModE").val(response1["gpu_modelo"]);
+			$("input.inputGPUCapE").val(response1["gpu_capacidad"]);
+
+			if (response1["mouse"] == 1) 
+			{$('.checkMouseE').prop('checked', true);}
+
+			if (response1["teclado"] == 1) 
+			{$('.checkTecladoE').prop('checked', true);}
+
+			$("#dateIngresoE").val(response1["fecha_ingreso"]);
+
+			$(".textObservacionesE").html(response1["observaciones"]);
+
+			if (response1["rol"] == 1) 
+			{
+				$(".selectRolE").append('<option value="1">Empleado</option>');
+				$(".selectRolE").append('<option value="0">Contratista</option>');
+								
+			}
+			else
+			{
+				$(".selectRolE").append('<option value="0">Contratista</option>');
+				$(".selectRolE").append('<option value="1">Empleado</option>');				
+			}
+
+
+			$(".selectIdCPUGenE").append('<option value="'+response1["cpu_generacion"]+'">'+response1["cpu_generacion"]+'</option>');
+			
+			for (var i = 12; i >= 4; i--) {
+				if (response1["cpu_generacion"] != i) 
+				{
+					$(".selectIdCPUGenE").append('<option value="'+i+'">'+i+'</option>');
+				}
+				
+			}
+
+			var elementos = [ "selectIdProE", "selectIdArqE", "selectIdMarcaE", "selectIdModeloE",
+			 "selectIdCPUE", "selectIdCPUModE", "selectSOE", "selectSOVerE", "selectIdActaE", "selectResponsableE", "selectAsignadoE",
+			 "selectProyectoE", "selectLicenciaE"];
+
+			var llaves = [ 'id_propietario', 'id_arquitectura', 'marca', 'modelo',
+			 'cpu', 'cpu_modelo', 'so', 'so_version', 'id_acta', 'id_responsable',
+			  'id_usuario', 'id_proyecto', 'id_licencia' ];
+
+			var valores = [ response1['id_propietario'], response1['id_arquitectura'], response1['marca'], 
+			response1['modelo'], response1['cpu'], response1['cpu_modelo'], 
+			response1['so'], response1['so_version'], response1['id_acta'], 
+			response1['id_responsable'], response1['id_usuario'], response1['id_proyecto'], response1['id_licencia'] ];
+
+			for (var i = 0; i < elementos.length; i++) 
+			{
+				var datas = new FormData();
+				datas.append("item" , llaves[i]);
+				datas.append("valor" , valores[i]);
+				datas.append("elemento" , elementos[i]);
+				datas.append("datosSelect", 1);
+
+				$.ajax({
+
+					url:"ajax/equipos.ajax.php",
+					method: "POST",
+					data: datas,
+					cache: false,
+					contentType: false,
+					processData: false,
+					dataType: "json",
+					success: function(response2)
+					{
+						console.log(" en #"+response2.length);
+						
+						$("#"+response2[response2.length-1]).children().remove();
+
+						for (var j = 0; j < response2.length-1; j++) 
+						{
+							$("#"+response2[response2.length-1]).append('<option value="'+response2[j]["id"]+'">'+response2[j]["nombre"]+'</option>');
+
+						}//for (var j = 0; j < arrI.length; j++)
+					}
+				});
+	
+			}
+
+		}
+	});
+
+});
 
 
 
